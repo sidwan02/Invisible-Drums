021944d68a276eca5e8fa2d4406d28b1d43fbe00
Siddharth Diwan <70694457+sidwan02@users.noreply.github.com>
2022-04-29 18:49:13 -0400

    attempt fix getting stuck in white using np.avg surrounding pixels

--- raft/meanshift.py
+++ raft/meanshift.py
@@ -52,7 +52,7 @@ def mark_centroids(grayscale, cluster_centers, img_name_suff=""):
     for (r, c) in cluster_centers:
         r, c = round(r), round(c)
         grayscale_w_centroids = cv2.circle(
-            grayscale_w_centroids, (c, r), radius=1, color=(0, 0, 255), thickness=10
+            grayscale_w_centroids, (c, r), radius=3, color=(0, 0, 255), thickness=-1
         )
 
     cv2.imwrite(
@@ -76,6 +76,8 @@ def mean_shift_custom(image, n_points=50, n_iter=50, radius=50):
         ]
     )
 
+    radius_orig = radius
+
     # print(f"points: {points}")
 
     # points = np.array([[300, 200]])
@@ -87,38 +89,47 @@ def mean_shift_custom(image, n_points=50, n_iter=50, radius=50):
     while cur_iter <= n_iter:
         print(f"cur_iter: {cur_iter}")
         for i, (r, c) in enumerate(points):
-            r, c = round(r), round(c)
+            loop_count = 1
+            radius = radius_orig
+            did_not_shift = True
+            while did_not_shift and loop_count <= 50:
+                r, c = round(r), round(c)
 
-            min_r, max_r = max(0, r - radius), min(h - 1, r + radius)
-            min_c, max_c = max(0, c - radius), min(w - 1, c + radius)
+                min_r, max_r = max(0, r - radius), min(h - 1, r + radius)
+                min_c, max_c = max(0, c - radius), min(w - 1, c + radius)
 
-            # print(f"surr_r: [{min_r}, {max_r}]")
-            # print(f"surr_c: [{min_c}, {max_c}]")
+                # print(f"surr_r: [{min_r}, {max_r}]")
+                # print(f"surr_c: [{min_c}, {max_c}]")
 
-            a = np.array(
-                [
-                    [surr_r, surr_c]
-                    for surr_r in range(min_r, max_r + 1)
-                    for surr_c in range(min_c, max_c + 1)
-                ]
-            )
-            # 255 => white; # 0 => black
-            # b = 255 - np.array(image[min_r : max_r + 1, min_c : max_c + 1]).flatten()
+                a = np.array(
+                    [
+                        [surr_r, surr_c]
+                        for surr_r in range(min_r, max_r + 1)
+                        for surr_c in range(min_c, max_c + 1)
+                    ]
+                )
+                # 255 => white; # 0 => black
+                # b = 255 - np.array(image[min_r : max_r + 1, min_c : max_c + 1]).flatten()
 
-            b = 255 - np.array(
-                [
-                    image[surr_r, surr_c]
-                    for surr_r in range(min_r, max_r + 1)
-                    for surr_c in range(min_c, max_c + 1)
-                ]
-            )
+                b = 255 - np.array(
+                    [
+                        image[surr_r, surr_c]
+                        for surr_r in range(min_r, max_r + 1)
+                        for surr_c in range(min_c, max_c + 1)
+                    ]
+                )
 
-            # print(f"a.shape: {a.shape}, b.shape: {b.shape}")
+                # print(f"a.shape: {a.shape}, b.shape: {b.shape}")
 
-            if sum(b) != 0:
-                points[i] = sum(a * np.expand_dims(b, axis=1)) / sum(b)
+                # if sum(b) != 0:
+                if np.average(b) > 50:
+                    points[i] = sum(a * np.expand_dims(b, axis=1)) / sum(b)
+                    did_not_shift = False
+                else:
+                    radius *= 2
+                    loop_count += 1
 
-            # if the sum is 0 that means all surrounding pixels are pure white 255. For now if that's the case don't update points which makes sense logically speaking
+                # if the sum is 0 that means all surrounding pixels are pure white 255. For now if that's the case don't update points which makes sense logically speaking
 
         mark_centroids(copy.deepcopy(image), points, img_name_suff=f"-{cur_iter}")
 
