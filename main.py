2fcf16f3232a5cf2d6c38a8a2a21c1c818ccbcc1
Siddharth Diwan <70694457+sidwan02@users.noreply.github.com>
2022-05-01 13:11:33 -0400

    merged run.py into main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from aggregator import aggregate_centroids, get_confidence_score
 
 
 def run_single_iteration(cluster_centroids):
-    '''
+    """
     runs our system for a single iteration
 
     Args:
         clusters: list of (row, col, intensity) tuples, one for each of the k cluster centroids
     Returns
         Nothing
-    '''
+    """
     # step 3: get blobs (aggregated clusters) and calculate the movement confidence score for this frame
     blob_locs, blob_intensities = aggregate_centroids(curr_frame_centroids, curr_frame)
-    confidence_score = get_confidence_score() # scalar
+    confidence_score = get_confidence_score()  # scalar
 
     # update state of prev scores
     n_prev_scores.append(confidence_score)
@@ -24,21 +24,31 @@ def run_single_iteration(cluster_centroids):
         m_prev_scores.pop(0)
 
     # step 4: detect potential rebound
-    curr_frame_has_potential_rebound, highest_intensity_pixel_loc = detect_potential_rebound(n_prev_scores, blob_locs, blob_intensities)
+    (
+        curr_frame_has_potential_rebound,
+        highest_intensity_pixel_loc,
+    ) = detect_potential_rebound(n_prev_scores, blob_locs, blob_intensities)
     # update state of prev rebounds
     prev_potential_rebounds.append(curr_frame_has_potential_rebound)
-    if len(prev_potential_rebounds) == 2*n:
+    if len(prev_potential_rebounds) == 2 * n:
         prev_potential_rebounds.pop(0)
 
     # step 5: detect rebound
-    rebound_location = detect_rebound(m_prev_scores, prev_potential_rebounds, blob_locs, blob_intensities)
+    rebound_location = detect_rebound(
+        m_prev_scores, prev_potential_rebounds, blob_locs, blob_intensities
+    )
     if rebound_location is not None:
         # step 6: get sound effect ID based on rebound location and play the sound
         sound_id = get_drum_sound(rebound_location)
         play_sound(sound_id)
 
         # (step 7): if we have a rebound, prevent a rebound in the next 5-10 frames?
-        
+
+
+import sys
+
+sys.path.append("raft/")
+from centroids import get_centroids
 
 if __name__ == "__main__":
     # set parameters
@@ -46,15 +56,30 @@ if __name__ == "__main__":
     m = 3
     T = 50
 
-    # step 1/2: feed in frames to RAFT and cluster by intensity
-    all_centroid_data = ___()
+    # step 1: generate image flows from RAFT
+    # See README.md: Generate Image Flows
+
+    # step 2: cluster image flows by intensity
+    all_centroid_data = get_centroids()
+    printf(f"all_centroid_data: {all_centroid_data}")
+    # dim 1 -> folder
+    # dim 2 -> video
+    # dim 3 -> frame
+    # dim 4 -> [r, c, intensity]
+
+    # Note: x = c, y = r
+
+    # intensity:
+    # 0 => 0 velocity
+    # 255 => highest velocity
 
     for folder in all_centroid_data:
         for video in folder:
             for frame_clusters in video:
                 # frame_clusters = [(r,c,intensity), ...]
                 # keep track of previous state
-                n_prev_scores = [] # scalar list
-                m_prev_scores = [] # scalar list
-                prev_potential_rebounds = [] # boolean list
-                run_single_iteration(frame_clusters) 
+                n_prev_scores = []  # scalar list
+                m_prev_scores = []  # scalar list
+                prev_potential_rebounds = []  # boolean list
+                run_single_iteration(frame_clusters)
+
