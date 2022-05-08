801591923282a098d675a5736cd604c8bd027282
Siddharth Diwan <70694457+sidwan02@users.noreply.github.com>
2022-05-07 21:58:26 -0400

    separated main from name function in centroids + added logic to save all_blobs_data.npy

--- centroids.py
+++ centroids.py
@@ -24,7 +24,13 @@ def load_image(imfile, resolution=None):
 
 def thing(args):
 
-    imfile1, centroids_dir_path, meanshift_dir_path, blobs_dir_path = args
+    (
+        imfile1,
+        centroids_dir_path,
+        meanshift_dir_path,
+        blobs_dir_path,
+        blobs_data_dir_path,
+    ) = args
 
     image1 = np.array(load_image(imfile1))
     svfile = imfile1
@@ -34,6 +40,7 @@ def thing(args):
     centroids_path = os.path.join(centroids_dir_path, os.path.basename(svfile))
     meanshift_path = os.path.join(meanshift_dir_path, os.path.basename(svfile))
     blobs_path = os.path.join(blobs_dir_path, os.path.basename(svfile))
+    blobs_data_path = os.path.join(blobs_data_dir_path, os.path.basename(svfile))
 
     grayscale = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     # cv2.imwrite("grayscale.png", grayscale)
@@ -53,6 +60,96 @@ def thing(args):
 
     # all_blobs_data.append(blobs_data) =================
 
+    np.save(blobs_data_dir_path[:-4] + "-blobs_data.npy", blobs_data)
+
+
+def main(args):
+    data_path = args.path
+
+    print(f"data_path: {data_path}")
+
+    img_flo_path = data_path + "/FlowImages_gap1"  # path to the dataset
+    superfolder = glob.glob(os.path.join(img_flo_path, "*"))
+
+    # folder = superfolder[0]
+    for folder in superfolder:
+
+        centroids_root = data_path + f"/Centroids/"
+        meanshift_root = data_path + f"/Mean-Shift/"
+        blobs_root = data_path + f"/Blobs/"
+        blobs_data_root = data_path + f"/Blobs-Data/"
+
+        all_blobs_data = []
+        # all_flow_grayscale = []
+        img_path1 = os.path.join(folder, "*.png")
+        img_path2 = os.path.join(folder, "*.jpg")
+        images = glob.glob(img_path1) + glob.glob(img_path2)
+
+        # print(f"images: {images}")
+
+        f = os.path.basename(folder)
+
+        centroids_dir_path = os.path.join(centroids_root, f)
+        meanshift_dir_path = os.path.join(meanshift_root, f)
+        blobs_dir_path = os.path.join(blobs_root, f)
+        blobs_data_dir_path = os.path.join(blobs_data_root, f)
+
+        # print(f"centroids_dir_path: {centroids_dir_path}")
+        # print(f"meanshift_dir_path: {meanshift_dir_path}")
+
+        if args.clear and os.path.exists(centroids_dir_path):
+            shutil.rmtree(centroids_dir_path)
+        os.makedirs(centroids_dir_path, exist_ok=True)
+
+        if args.clear and os.path.exists(meanshift_dir_path):
+            shutil.rmtree(meanshift_dir_path)
+        os.makedirs(meanshift_dir_path, exist_ok=True)
+
+        if args.clear and os.path.exists(blobs_dir_path):
+            shutil.rmtree(blobs_dir_path)
+        os.makedirs(blobs_dir_path, exist_ok=True)
+
+        if args.clear and os.path.exists(blobs_data_dir_path):
+            shutil.rmtree(blobs_data_dir_path)
+        os.makedirs(blobs_data_dir_path, exist_ok=True)
+
+        # images_ = sorted(images)
+
+        # zipped = zip(images, )
+
+        pool = Pool()
+        pool.map(
+            thing,
+            zip(
+                images,
+                repeat(centroids_dir_path),
+                repeat(meanshift_dir_path),
+                repeat(blobs_dir_path),
+                repeat(blobs_data_dir_path),
+            ),
+        )
+
+        # blobs_data_folders.append(all_blobs_data) =================
+        # flow_grayscale_folders.append(all_flow_grayscale)
+
+        # np.save(data_path + "/all_flow_grayscale.npy", flow_grayscale_folders)
+        # print(cluster_centers_folders)
+        # return cluster_centers_folders
+
+    all_folders_blobs_data = []
+    for folder in superfolder:
+        folder_blob_data = []
+        npy_path = os.path.join(folder, "*.npy")
+        folder_blob_data_paths = glob.glob(npy_path)
+
+        for blob_data_path in folder_blob_data_paths:
+            blob_data = np.load(blob_data_path, allow_pickle=True)
+            folder_blob_data.append(blob_data)
+
+        all_folders_blobs_data.append(folder_blob_data)
+
+    np.save(data_path + "/all_blobs_data.npy", all_folders_blobs_data)
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -62,73 +159,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # main(args)
-
-    # ==================================
-
-    # os.system(f"python run_inference.py")
-    data_path = args.path
-
-    print(f"data_path: {data_path}")
-
-    img_flo_path = data_path + "/FlowImages_gap1"  # path to the dataset
-    superfolder = glob.glob(os.path.join(img_flo_path, "*"))
-
-    folder = superfolder[0]
-
-    centroids_root = data_path + f"/Centroids/"
-    meanshift_root = data_path + f"/Mean-Shift/"
-    blobs_root = data_path + f"/Blobs/"
-
-    all_blobs_data = []
-    # all_flow_grayscale = []
-    img_path1 = os.path.join(folder, "*.png")
-    img_path2 = os.path.join(folder, "*.jpg")
-    images = glob.glob(img_path1) + glob.glob(img_path2)
-
-    # print(f"images: {images}")
-
-    f = os.path.basename(folder)
-
-    centroids_dir_path = os.path.join(centroids_root, f)
-    meanshift_dir_path = os.path.join(meanshift_root, f)
-
-    blobs_dir_path = os.path.join(blobs_root, f)
-
-    # print(f"centroids_dir_path: {centroids_dir_path}")
-    # print(f"meanshift_dir_path: {meanshift_dir_path}")
-
-    if args.clear and os.path.exists(centroids_dir_path):
-        shutil.rmtree(centroids_dir_path)
-    os.makedirs(centroids_dir_path, exist_ok=True)
-
-    if args.clear and os.path.exists(meanshift_dir_path):
-        shutil.rmtree(meanshift_dir_path)
-    os.makedirs(meanshift_dir_path, exist_ok=True)
-
-    if args.clear and os.path.exists(blobs_dir_path):
-        shutil.rmtree(blobs_dir_path)
-    os.makedirs(blobs_dir_path, exist_ok=True)
-
-    # images_ = sorted(images)
-
-    # zipped = zip(images, )
-
-    pool = Pool()
-    pool.map(
-        thing,
-        zip(
-            images,
-            repeat(centroids_dir_path),
-            repeat(meanshift_dir_path),
-            repeat(blobs_dir_path),
-        ),
-    )
-
-    # blobs_data_folders.append(all_blobs_data) =================
-    # flow_grayscale_folders.append(all_flow_grayscale)
-
-    # np.save(data_path + "/all_blobs_data.npy", blobs_data_folders) ===================
-    # np.save(data_path + "/all_flow_grayscale.npy", flow_grayscale_folders)
-    # print(cluster_centers_folders)
-    # return cluster_centers_folders
+    main(args)
